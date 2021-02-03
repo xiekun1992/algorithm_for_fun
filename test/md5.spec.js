535c8ddda56a64552b6034eecf74b21af4413667
xiekun1992 <xkfront@outlook.com>
2021-02-03 22:56:06 +0800

    change: add string test

--- test/md5.spec.js
+++ test/md5.spec.js
@@ -44,15 +44,28 @@ describe('MD5', () => {
     const buf = Buffer.alloc(10 * 1024 * 1024, Math.random())
     assert.equal.apply(null, calcHelper(buf, md5, xmd5))
   })
-  // it('', () => {
-  //   MD5 ("") = d41d8cd98f00b204e9800998ecf8427e
-  //   MD5 ("a") = 0cc175b9c0f1b6a831c399e269772661
-  //   MD5 ("abc") = 900150983cd24fb0d6963f7d28e17f72
-  //   MD5 ("message digest") = f96b697d7cb7938d525a2f31aaf161d0
-  //   MD5 ("abcdefghijklmnopqrstuvwxyz") = c3fcd3d76192e4007dfb496cca67e13b
-  //   MD5 ("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789") =
-  //   d174ab98d277d9f5a5611c2c9f419d9f
-  //   MD5 ("123456789012345678901234567890123456789012345678901234567890123456
-  //   78901234567890") = 57edf4a22be3c955ac49da2e2107b67a
-  // })
+  it('string `a`', () => {
+    const buf = Buffer.from('a')
+    assert.equal.apply(null, calcHelper(buf, md5, xmd5))
+  })
+  it('string `abc`', () => {
+    const buf = Buffer.from('abc')
+    assert.equal.apply(null, calcHelper(buf, md5, xmd5))
+  })
+  it('string `message digest`', () => {
+    const buf = Buffer.from('message digest')
+    assert.equal.apply(null, calcHelper(buf, md5, xmd5))
+  })
+  it('string `abcdefghijklmnopqrstuvwxyz`', () => {
+    const buf = Buffer.from('abcdefghijklmnopqrstuvwxyz')
+    assert.equal.apply(null, calcHelper(buf, md5, xmd5))
+  })
+  it('string `ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789`', () => {
+    const buf = Buffer.from('ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789')
+    assert.equal.apply(null, calcHelper(buf, md5, xmd5))
+  })
+  it('string `123456789012345678901234567890123456789012345678901234567890123456`', () => {
+    const buf = Buffer.from('123456789012345678901234567890123456789012345678901234567890123456')
+    assert.equal.apply(null, calcHelper(buf, md5, xmd5))
+  })
 })
